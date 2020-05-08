d2315afbbcaaff40dfe42f5c1d1533db67533f0f
Shuayb Khan <shuaybk@gmail.com>
2020-05-08 13:48:05 -0400

    Create options menu and add Icon for Settings menu (no action on click)

--- app/src/main/java/com/shuayb/capstone/android/poc_custom_settings/MainActivity.java
+++ app/src/main/java/com/shuayb/capstone/android/poc_custom_settings/MainActivity.java
@@ -3,6 +3,8 @@ package com.shuayb.capstone.android.poc_custom_settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
