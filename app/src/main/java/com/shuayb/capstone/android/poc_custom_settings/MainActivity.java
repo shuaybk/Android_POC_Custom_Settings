f055a40494edb614980202a17e82f5a4764180ee
Shuayb Khan <shuaybk@gmail.com>
2020-05-08 14:36:17 -0400

    Create Settings Activity with a basic ListPreference

--- app/src/main/java/com/shuayb/capstone/android/poc_custom_settings/MainActivity.java
+++ app/src/main/java/com/shuayb/capstone/android/poc_custom_settings/MainActivity.java
@@ -2,9 +2,11 @@ package com.shuayb.capstone.android.poc_custom_settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
